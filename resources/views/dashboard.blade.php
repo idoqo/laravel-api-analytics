e370406a1236bf63ab310de14f69310162080065
idoko <radpangolin@gmail.com>
2020-03-18 22:45:54 +0100

    scaffold vue and bootstrap with matching route

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>API Analytics</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet" />
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
