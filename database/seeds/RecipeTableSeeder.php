999206a83806a4083645643b99e0cdceb6464892
idoko <radpangolin@gmail.com>
2020-03-18 21:21:11 +0100

    update seed data

--- database/seeds/RecipeTableSeeder.php
+++ database/seeds/RecipeTableSeeder.php
@@ -8,23 +8,18 @@ class RecipeTableSeeder extends Seeder
     {
         \App\Recipe::insert([
             [
-                'title' => "Boiled Yam",
-                'body' => "- Cut the yam tuber into 1 inch slices.
-                - Peel and cut the slices into half moons or circular shapes.
-                - Wash the slices, place in a pot and pour water to cover the contents.
-                - Boil till the yam is soft. This is when you can easily drive a fork into the slices without resistance.
-                - Add salt and leave to cook for about 2 minutes.
-                - Turn off the heat and drain the water.
-                "
+                'title' => "Nigerian Jollof Rice with Chicken",
+                'body' => "Test Recipe. Chicken is first sauteed on the stove top to produce a wonderful aromatic base for the rice.",
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
             ],
             [
                 'title' => "Chicken Suya Salad",
-                'body' => "- Rinse all the vegetables thoroughly.
-                - Cut the pieces of chicken suya into thin stripes with a width of about half an inch (about 1.5cm).
-                - Cut thin strips of the cucumber along the fruit.
-                - Cut the tomatoes and cabbage.
-                - Arrange the vegetables like petals
-                "
+                'body' => "- Cut the yam tuber into 1 inch slices. - Peel and cut the slices into half moons.
+                        - Wash the slices, place in a pot and pour water to cover the contents.
+                        - Boil till the yam is soft.",
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now()
             ]
         ]);
     }
