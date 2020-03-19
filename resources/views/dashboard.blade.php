ef1ab15bb2d13d7236b68e40afc4da42c3fab81b
idoko <radpangolin@gmail.com>
2020-03-19 10:43:27 +0100

    remove example component and replace it with dashboard-component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 <div id="app">
-    <example-component></example-component>
+    <dashboard-component></dashboard-component>
 </div>
 <script src="{{mix('js/app.js')}}"></script>
 </body>
